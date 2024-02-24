2ff3747de903161d676f49c39e5257885618e1a6
Arnaud <arnaud-buhl@orange.fr>
2024-02-24 15:35:20 +0100

    feat: footer
    - create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+function Footer() {
+    return <footer>
+        <img src='./assets/logo.svg' alt="kasa logo"/>
+        <p>© 2024 Kasa. All rights reserved</p>
+    </footer>
+}
+
+export default Footer
