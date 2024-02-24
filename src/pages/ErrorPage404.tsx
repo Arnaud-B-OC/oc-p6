88bac65f1c5fcc84efd82f87df16942bf6d7a260
Arnaud <arnaud-buhl@orange.fr>
2024-02-24 15:35:36 +0100

    feat: err page 404
    - create error page

--- src/pages/ErrorPage404.tsx
+++ src/pages/ErrorPage404.tsx
@@ -1,6 +1,20 @@
+import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 function ErrorPage404() {
-    return <p>404 Error</p>
+    return <>
+        <Header/>
+
+        <main className='errPage'>
+            <p className='err404'>404</p>
+            <h1 className='err'>Oups! La page que vous demandez n'existe pas.</h1>
+
+            <Link to='/' className='err'>Retourner sur la page d’accueil</Link>
+        </main>
+
+        <Footer/>
+    </>
 }
 
 export default ErrorPage404
