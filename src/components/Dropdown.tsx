c857c6a3f5c7c108ef7c95e7e9df0ae344bb8840
Arnaud <arnaud-buhl@orange.fr>
2024-02-28 15:03:34 +0100

    feat(update): dropdown
    - change text type to string or string array

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,12 +1,14 @@
 
-function Dropdown({title, text} : {title : string, text : string}) {
+function Dropdown({title, text} : {title : string, text : string[] | string}) {
+    let data = Array.isArray(text) ? text : [text]
+    
     return <div className='dropdown'>
         <div className='bar'>
             <p>{title}</p>
             <img src='/assets/icons/arrow.svg' alt=''/>
         </div>
         <div className='content'>
-            <p>{text}</p>
+            {data.map((t) => { return <p>{t}</p> })}
         </div>
     </div>
 }
