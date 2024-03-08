047de57958b126fbbb07a3122c88335681da0cf8
Arnaud <arnaud-buhl@orange.fr>
2024-03-08 17:07:21 +0100

    refactor: error page style

--- src/pages/error/ErrorPage404.tsx
+++ src/pages/error/ErrorPage404.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+import Footer from '../../components/footer/Footer'
+import Header from '../../components/header/Header'
+import './errorPage.scss'
 
 function ErrorPage404() {
     return <>
