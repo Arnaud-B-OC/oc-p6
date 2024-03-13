1656f7f1abf4abb45b945de286a186fc11c75abb
Arnaud <arnaud-buhl@orange.fr>
2024-03-13 16:07:16 +0100

    fix: rating stars color

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -1,12 +1,12 @@
-import './rating.scss'
+import './rating.scss';
 
 function Rating({rating} : {rating : number}) {
     return <div className='rating'>
-        <img className={rating >= 1 ? 'star-active' : 'star-inactive'} src='/assets/icons/star.svg' alt=''/>
-        <img className={rating >= 2 ? 'star-active' : 'star-inactive'} src='/assets/icons/star.svg' alt=''/>
-        <img className={rating >= 3 ? 'star-active' : 'star-inactive'} src='/assets/icons/star.svg' alt=''/>
-        <img className={rating >= 4 ? 'star-active' : 'star-inactive'} src='/assets/icons/star.svg' alt=''/>
-        <img className={rating >= 5 ? 'star-active' : 'star-inactive'} src='/assets/icons/star.svg' alt=''/>
+        <img src={rating >= 1 ? '/assets/icons/star-active.svg' : '/assets/icons/star-inactive.svg'} alt=''/>
+        <img src={rating >= 2 ? '/assets/icons/star-active.svg' : '/assets/icons/star-inactive.svg'} alt=''/>
+        <img src={rating >= 3 ? '/assets/icons/star-active.svg' : '/assets/icons/star-inactive.svg'} alt=''/>
+        <img src={rating >= 4 ? '/assets/icons/star-active.svg' : '/assets/icons/star-inactive.svg'} alt=''/>
+        <img src={rating >= 5 ? '/assets/icons/star-active.svg' : '/assets/icons/star-inactive.svg'} alt=''/>
     </div>
 }
 
