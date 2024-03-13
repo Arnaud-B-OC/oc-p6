944421fdd3a6ac8b2d2b923804f0fd1ab2597ca3
Arnaud <arnaud-buhl@orange.fr>
2024-03-13 16:00:45 +0100

    feat: home page
    - replace old banner by banner components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,28 +2,22 @@ import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './home.scss'
+import './home.scss';
 import { LogementData } from '../../interfaces';
+import Banner from '../../components/banner/Banner';
 
 function Home() {
-    const [logements, setLogements] = useState<LogementData[]>([])
+    const [logements, setLogements] = useState<LogementData[]>([]);
 
     useEffect(() => {
-        fetch('http://localhost:3000/api/logements.json').then((data) => data.json()).then((json) => {
-
-            console.log(json)
-
-            setLogements(json)
-        });
-	}, [])
+        fetch('http://localhost:3000/api/logements.json').then((data) => data.json()).then((json) => setLogements(json));
+	}, []);
 
     return <>
         <Header/>
 
         <main className='home'>
-            <div className='homeImage'>
-                <h1>Chez vous, partout et ailleurs</h1>
-            </div>
+            <Banner title='Chez&nbsp;vous, partout&nbsp;et&nbsp;ailleurs' image='/assets/images/img1.png'/>
 
             <section className='logements'>
                 {logements.map((logement) => {
