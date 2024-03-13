ddd9389126d77e6704988ac85df3826508d48a99
Arnaud <arnaud-buhl@orange.fr>
2024-03-13 01:19:58 +0100

    feat: logement page
    - update style
    - get data with request

--- src/pages/logement/Logement.tsx
+++ src/pages/logement/Logement.tsx
@@ -1,88 +1,62 @@
-import Dropdown from "../../components/dropdown/Dropdown"
-import Footer from "../../components/footer/Footer"
-import Header from "../../components/header/Header"
-import Tag from "../../components/tag/Tag"
+import { useEffect, useState } from 'react'
+import Dropdown from '../../components/dropdown/Dropdown'
+import Footer from '../../components/footer/Footer'
+import Header from '../../components/header/Header'
+import Host from '../../components/host/Host'
+import Rating from '../../components/rating/Rating'
+import Tag from '../../components/tag/Tag'
+import { LogementData } from '../../interfaces'
+import { useParams } from 'react-router-dom'
+import Carrousel from '../../components/carrousel/Carrousel'
 import './logement.scss'
+import ErrorPage404 from '../error/ErrorPage404'
 
 function Logement() {
-    
-    const data = {
-		"id": "c67ab8a7",
-		"title": "Appartement cosy",
-		"cover": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
-		"pictures": [
-			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
-			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
-			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
-			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
-			"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"
-		],
-		"description": "Votre maison loin de chez vous. Que vous veniez de l'autre bout du monde, ou juste de quelques stations de RER, vous vous sentirez chez vous dans notre appartement.",
-		"host": {
-			"name": "Nathalie Jean",
-			"picture": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg"
-		},
-		"rating": "5",
-		"location": "Ile de France - Paris 17e",
-		"equipments": [
-			"Équipements de base",
-			"Micro-Ondes",
-			"Douche italienne",
-			"Frigo",
-			"WIFI"
-		],
-		"tags": [
-			"Batignolle",
-			"Montmartre"
-		]
-	}
+    const params = useParams();
+
+    const [logement, setLogement] = useState<LogementData>();
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/logements.json').then((data) => data.json()).then((json) => {
+            let logement : LogementData | undefined = json.find((logement : LogementData) => logement.id === params.id);
+            setLogement(logement);
+        });
+	}, [params.id]);
 
     return <>
-        <Header/>
+        {logement && <>
+            <Header/>
 
-        <main>
-            {/* TODO : Carrousel Component */}
-            <div>
-                {data.pictures.map((picture) => {
-                    return <img src={picture} style={{display: 'none'}} key={picture} alt=''/>
-                })}
-            </div>
+            <main>
+                <Carrousel images={logement?.pictures}/>
+                
+                <section className='logement'>
+                    <div className='upperlogementcontainer'>
+                        <div className='hostratingtitle'>
+                            <h1>{logement?.title}</h1>
+                            <h2>{logement?.location}</h2>
+                            
+                            <div className='tags-list'>
+                                {logement?.tags.map((tag) => <Tag key={tag} name={tag}/>)}
+                            </div>
+                        </div>
 
-            <section className='logement'>
-                <div className='upperlogementcontainer'>
-                    <div className='hostratingtitle'>
-                        <h1>{data.title}</h1>
-                        <h2>{data.location}</h2>
-                        
-                        <div className='tags-list'>
-                            {data.tags.map((tag) => {
-                                return <Tag key={tag} name={tag}/>
-                            })}
+                        <div className='host-rating'>
+                            <Host fullname={logement?.host.name ?? ''} picture={logement?.host.picture ?? ''}/>
+                            <Rating rating={logement?.rating ?? 0}/>
                         </div>
                     </div>
 
-                    <div className='host-rating'>
-                        {/* TODO : Host */}
-                        <div className='host'>
-                            <p>{data.host.name}</p>
-                            <img src={data.host.picture} alt={data.host.name}/>
-                        </div>
-                        
-                        {/* TODO : Rating */}
-                        <div className='rating'>
-                            <p>{data.rating} étoiles</p>
-                        </div>
+                    <div className='logement-details'>
+                        <Dropdown title='Description' text={logement?.description ?? ''}/>
+                        <Dropdown title='Équipements' text={logement?.equipments ?? ''}/>
                     </div>
-                </div>
+                </section>
+            </main>
 
-                <div className='logement-details'>
-                    <Dropdown title='Description' text={data.description}/>
-                    <Dropdown title='Équipements' text={data.equipments}/>
-                </div>
-            </section>
-        </main>
-
-        <Footer/>
+            <Footer/>
+        </>}
+        {!logement && <ErrorPage404/>}
     </>
 }
 
