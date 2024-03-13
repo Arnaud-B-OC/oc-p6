eedfeb2d2ce008cbbdb014b5a12a753def05dcf3
Arnaud <arnaud-buhl@orange.fr>
2024-03-13 15:56:22 +0100

    feat: about page
    - replace banner
    - add request to get data
    - add interface AboutData
    - add about.json

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -14,3 +14,8 @@ export interface LogementData {
     tags: string[]
     title: string
 }
+
+export interface AboutData {
+    title: string
+    text: string
+}
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,21 +1,28 @@
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import Dropdown from '../../components/dropdown/Dropdown';
-import './about.scss'
+import './about.scss';
+import { useEffect, useState } from 'react';
+import { AboutData } from '../../interfaces';
+import Banner from '../../components/banner/Banner';
 
 function About() {
+    const [aboutData, setAboutData] = useState<AboutData[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/about.json').then((data) => data.json()).then((json) => {
+            setAboutData(json)
+        });
+	}, []);
+
     return <>
         <Header/>
 
         <main>
-            <div className='aboutImage'>
-            </div>
+            <Banner image='/assets/images/img2.png'/>
 
             <section className='about'>
-                <Dropdown title='Fiabilité' text='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.'/>
-                <Dropdown title='Respect' text='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'/>
-                <Dropdown title='Service' text='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'/>
-                <Dropdown title='Sécurité' text='La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'/>
+                {aboutData.map((aboutItem, index) => <Dropdown key={index} title={aboutItem.title} text={aboutItem.text}/>)}
             </section>
         </main>
 
