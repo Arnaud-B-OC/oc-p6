4bdc8204797361a6152b94997d45edf56181ffbe
Arnaud <arnaud-buhl@orange.fr>
2024-03-16 15:31:39 +0100

    feat: dropdown open/close
    - add toggle function
    - replace text array map by join
    - update style

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,15 +1,20 @@
+import { useRef, useState } from 'react';
 import './dropdown.scss';
 
-function Dropdown({title, text} : {title : string, text : string[] | string} = {title: '', text: []}) {
-    let data = Array.isArray(text) ? text : [text]
-    
-    return <div className='dropdown'>
+function Dropdown({title, text} : {title?: string, text: string[] | string}) {
+    const [state, setState] = useState<number>(0);
+    const data = Array.isArray(text) ? text : [text];
+
+    const contentDiv = useRef<HTMLDivElement>(null);
+    const toggleDropdown = () => setState(state === 0 ? contentDiv.current!.scrollHeight : 0);
+
+    return <div className={`dropdown${state ? ' open' : ''}`}>
         <div className='bar'>
             <p>{title}</p>
-            <img src='/assets/icons/arrow.svg' alt=''/>
+            <button onClick={toggleDropdown}><img src='/assets/icons/arrow.svg' alt=''/></button>
         </div>
-        <div className='content'>
-            {data.map((t) => { return <p key={t}>{t}</p> })}
+        <div className='content' ref={contentDiv} style={{maxHeight: state}}>
+            <p>{data.join('\n')}</p>
         </div>
     </div>
 }
