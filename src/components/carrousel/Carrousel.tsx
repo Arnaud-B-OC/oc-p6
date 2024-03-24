5d5ef0285d55607eb79c726be07061a3257c83de
Arnaud <arnaud-buhl@orange.fr>
2024-03-24 14:42:25 +0100

    fix: carrousel next prev counter

--- src/components/carrousel/Carrousel.tsx
+++ src/components/carrousel/Carrousel.tsx
@@ -6,8 +6,8 @@ function Carrousel({images} : {images: string[] | undefined}) {
     const imgs = images ?? [];
     const showImageNav = imgs.length !== 1;
 
-    const prevSlide = () => setSlide(0 < (slide - 1) ? (slide - 1) : (imgs.length - 1));
-    const nextSlide = () => setSlide(imgs.length > (slide + 1) ? (slide + 1) : 0);
+    const prevSlide = () => setSlide(slide > 0 ? (slide - 1) : (imgs.length - 1));
+    const nextSlide = () => setSlide(slide < (imgs.length - 1) ? (slide + 1) : 0);
     
     return <div className='carrousel'>
         {imgs.map((image, index) => <img key={index} src={image} className={slide === index ? 'slide' : 'hidden'} alt=''/>)}
